a0fa80879f462d7c11466e6539dd6bf930221fe8
Ankit Raj <ankitraj07032002@gmail.com>
2026-02-20 03:43:49 +0530

    Add frontend Next.js app

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,18 @@
+
+export default function NotFound() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[#020202] text-white text-center px-6">
+      <h1 className="text-6xl font-bold mb-4">404</h1>
+      
+      <h2 className="text-2xl font-semibold mb-2">
+        Page Not Found
+      </h2>
+
+      <p className="text-gray-400 mb-6 max-w-md">
+        The page you&apos;re looking for doesn’t exist or has been moved.
+      </p>
+
+     
+    </div>
+  );
+}
