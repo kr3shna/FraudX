5dfd1034566b12a632a6542dd41e22c0a6f7a183
Ankit Raj <ankitraj07032002@gmail.com>
2026-02-20 08:06:49 +0530

    added all frontend files

--- frontend/app/client/axiosClient.ts
+++ frontend/app/client/axiosClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiUrl =
   typeof process !== 'undefined'
     ? (process.env.NEXT_PUBLIC_API_URL ?? 'http://localhost:8000')
-    : 'http://localhost:8000';
+    : 'http://localhost:8000' ;
 
 const axiosClient = axios.create({
   baseURL: apiUrl,
